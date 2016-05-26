7e047ddb854274388b303cb38ad3cebc5a397391
polest <a.jaks@gmx.de>
2016-05-26 20:58:04 +0200

    dreieck

--- minigolf/dreieck.cpp
+++ minigolf/dreieck.cpp
@@ -1,6 +1,45 @@
 #include "dreieck.h"
 
-Dreieck::Dreieck()
+Dreieck::Dreieck(QWidget *parent)
+    : QOpenGLWidget(parent)
 {
+}
 
+Dreieck::~Dreieck()
+{
+}
+
+void Dreieck::initializeGL()
+{
+    initializeOpenGLFunctions();
+
+    glClearColor(0,0,0,1);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHTING);
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+    glEnable(GL_COLOR_MATERIAL);
+}
+
+void Dreieck::paintGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+        glColor3f(1.0, 0.0, 0.0);
+        glVertex3f(-0.5, -0.5, 0);
+        glColor3f(0.0, 1.0, 0.0);
+        glVertex3f( 0.5, -0.5, 0);
+        glColor3f(0.0, 0.0, 1.0);
+        glVertex3f( 0.0,  0.5, 0);
+    glEnd();
+}
+
+void Dreieck::resizeGL(int w, int h)
+{
+    glViewport(0,0,w,h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 }
--- minigolf/dreieck.h
+++ minigolf/dreieck.h
@@ -1,11 +1,22 @@
 #ifndef DREIECK_H
 #define DREIECK_H
 
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
 
-class Dreieck
+class Dreieck : public QOpenGLWidget, protected QOpenGLFunctions
 {
+    Q_OBJECT
+
 public:
-    Dreieck();
+    Dreieck(QWidget *parent = 0);
+    ~Dreieck();
+
+protected:
+    void initializeGL();
+    void resizeGL(int w, int h);
+    void paintGL();
+
 };
 
 #endif // DREIECK_H
