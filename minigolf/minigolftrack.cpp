4b75ad74a4670e7d4299df17b95482b1b068df02
Tabbidabbi <winkler.bremen@hotmail.com>
2016-06-08 15:25:15 +0200

    walls die zweite

--- minigolf/minigolftrack.cpp
+++ minigolf/minigolftrack.cpp
@@ -15,15 +15,30 @@ void minigolfTrack::drawTrack() {
         viereck.drawLeftWall(1,1,1);
 
         glTranslatef(0,0,4);
+
         viereck.drawGround(0,102,0);
         viereck.drawRightWall(1,1,1);
         viereck.drawLeftWall(1,1,1);
+
         glTranslatef(0,0,4);
-          viereck.drawGround(0,102,0);
-           glTranslatef(0,0,4);
-           viereck.drawGround(0,102,0);
-           glTranslatef(4,0,0);
-           viereck.drawGround(0,102,0);
+
+        viereck.drawGround(0,102,0);
+        viereck.drawRightWall(1,1,1);
+        viereck.drawLeftWall(1,1,1);
+
+        glTranslatef(0,0,4);
+
+        viereck.drawGround(0,102,0);
+        viereck.drawRightWall(1,1,1);
+        viereck.drawNorthWall(1,1,1);
+
+        glTranslatef(4,0,0);
+
+        viereck.drawGround(0,102,0);
+        viereck.drawNorthWall(1,1,1);
+        viereck.drawSouthWall(1,1,1);
+         viereck.drawLeftWall(1,1,1);
+
 
 
 
