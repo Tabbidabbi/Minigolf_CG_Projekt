914b21ffa67aad8cfc2064b820e9958f5992898c
Tabbidabbi <winkler.bremen@hotmail.com>
2016-06-11 11:58:39 +0200

    strength slider

--- minigolf/oglwidget.cpp
+++ minigolf/oglwidget.cpp
@@ -123,7 +123,7 @@ void OGLWidget::paintGL()
     // glPushMatrix, glPopMatrix - Legen die aktuelle Matrix auf den Stack bzw. nehmen sie wieder herunter.
     //glPushMatrix();
       glRotatef(90, 1, 0, 0);
-      glRotatef(-180, 0, 1, 0);
+      glRotatef(180, 0, 1, 0);
 
     //Bestimmt einen symbolischen Wert der eine Färbetechnik (Shadingtechnique) repräsentiert. Akzeptierte Werte sind GL_FLAT und GL_SMOOTH.
     glShadeModel(GL_FLAT);
